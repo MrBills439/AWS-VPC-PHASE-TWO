455b8cb19a8d1f0c790881e6cdee8879078698e3
Felix Appiah-Kubi <feddybills17@outlook.com>
2026-01-20 14:29:44 +0000

    added main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,60 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 
   tags = {
-    Name = "${var.project}-vpc"
+    Name = "learning-vpc"
   }
+}
+
+#-------------------------
+# Internet Gateway
+#-------------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "learning-igw"
+  }
+}
+
+#-------------------------
+# Public Subnet
+#-------------------------
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "learning-public-subnet"
+  }
+}
+
+
+#----------------------------
+# Route Table
+#----------------------------
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "learning-public-route-table"
+  }
+}
+
+#------------------------------
+# Route Table Association
+#------------------------------
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
