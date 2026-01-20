ac8cc38534157bba22e2c46d92df0c6709adf6da
Felix Appiah-Kubi <feddybills17@outlook.com>
2026-01-20 14:42:47 +0000

    building the public subnet

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+    description = "Aws region to deploy resources into"
+    type        = string
+    default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC"
+    type        = string
+    default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR block for the public subnet"
+    type        = string
+    default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+    description = "Availability zone for the subnet"
+    type        = string
+    default     = "us-east-1a"
+}
+
+variable "project_name" {
+    description = "AZ for the public subnet"
+    type        = string
+    default     = "learning-vpc"
+}
+
